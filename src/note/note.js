4d472582dd89d386840c5c9d8db97675b6738e8e
Pavel Shur <pavel6355832@tut.by>
2018-03-18 10:36:48 +0200

    Add note logic and style

--- src/note/note.js
+++ src/note/note.js
@@ -7,9 +7,50 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var note_item_1 = require("../item/note.item");
+var dialog;
 var NoteComponent = /** @class */ (function () {
     function NoteComponent() {
+        /**
+          * Поле, которое хранит в себе массив элементов списка
+          */
+        this.items = [];
     }
+    NoteComponent.prototype.ngAfterViewInit = function () {
+        dialog = document.querySelector('dialog');
+        document.querySelector('#show').onclick = function () {
+            dialog.showModal();
+        };
+        document.querySelector('.close').onclick = function () {
+            dialog.close();
+        };
+    };
+    /**
+     * Метод, который добавляет записку
+     * @param=textN строка, которую нужно добавить
+     * @param=dateN дата, которую нужно добавить
+     * @param=nameN автор, которого нужно добавить
+     */
+    NoteComponent.prototype.addItem = function (textN, dateN, nameN) {
+        var lastItem;
+        var id;
+        if (this.items.length == 0)
+            id = 1;
+        else {
+            lastItem = this.items[this.items.length - 1];
+            id = lastItem.id + 1;
+        }
+        this.items.push(new note_item_1.NoteItem(id, textN, dateN, nameN));
+    };
+    NoteComponent.prototype.removeItem = function (id) {
+        var newItems = [];
+        for (var _i = 0, _a = this.items; _i < _a.length; _i++) {
+            var item = _a[_i];
+            if (item.id != id)
+                newItems.push(new note_item_1.NoteItem(item.id, item.textNote, item.dateOfBegin, item.autor));
+        }
+        this.items = newItems;
+    };
     NoteComponent = __decorate([
         core_1.Component({
             selector: 'note-app',
--- src/note/note.ts
+++ src/note/note.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NoteItem } from '../item/note.item'; 
+
+var dialog;
 
 @Component({
     selector: 'note-app',
@@ -8,6 +11,62 @@ import { Router } from '@angular/router';
     providers: []
 })
 
-export class NoteComponent{} 
+export class NoteComponent{
+
+	  ngAfterViewInit(){
+      dialog = document.querySelector('dialog');
+  		document.querySelector('#show').onclick = function() {
+  		  dialog.showModal();
+  		};
+  		document.querySelector('.close').onclick = function() {
+  		  dialog.close();
+  		};
+    }
+
+    /** 
+      * Поле, которое хранит в себе массив элементов списка
+      */
+    items: NoteItem[]=[];
+
+
+     /** 
+      * Метод, который добавляет записку
+      * @param=textN строка, которую нужно добавить
+      * @param=dateN дата, которую нужно добавить
+      * @param=nameN автор, которого нужно добавить
+      */
+    addItem(textN: string, dateN: Date, nameN: string): void {
+      
+      var lastItem;
+      let id;
+      if (this.items.length == 0) 
+        id = 1;
+      else
+      {
+        lastItem = this.items[this.items.length - 1];
+        id = lastItem.id+1;
+      }
+      
+      this.items.push(new NoteItem(id, textN, dateN, nameN));
+    }
+
+    removeItem(id: number): void {
+      
+      let newItems : NoteItem[]=[];
+      for (var item of this.items) {
+        if (item.id != id)
+          newItems.push(new NoteItem(item.id, item.textNote,  item.dateOfBegin, item.autor));
+       }
+      this.items=newItems;
+    }
+
+
+
+
+
+
+
+
+} 
 
 
